a16b52e0da9410f9b148731999a1bf500615d1bd
Sharbatanu Chatterjee <sharbatanu444@gmail.com>
2019-01-27 14:35:01 +0100

    add code

--- action-how-are-you.py
+++ action-how-are-you.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
 
-INTENT_HOW_ARE_YOU = "bezzam:how_are_you"
+INTENT_HOW_ARE_YOU = "sharbatc:how_are_you"
 
 
 def main():
@@ -14,7 +14,7 @@ def main():
 def how_are_you_callback(hermes, intent_message):
     session_id = intent_message.session_id
     response = "I'm doing great."
-    hermes.publish_end_session(session_id, response)
+    hermes.publish_end_session(session_id, response)a
 
 
 if __name__ == "__main__":
